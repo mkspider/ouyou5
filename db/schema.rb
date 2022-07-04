3d51ca4191cb956b4e6b98783b75f9154378748a
EC2 Default User <ec2-user@ip-172-31-18-135.ap-northeast-1.compute.internal>
2022-07-04 10:55:32 +0000

    コメント

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_30_082346) do
+ActiveRecord::Schema.define(version: 2022_07_02_013743) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -40,6 +40,14 @@ ActiveRecord::Schema.define(version: 2022_06_30_082346) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -53,6 +61,8 @@ ActiveRecord::Schema.define(version: 2022_06_30_082346) do
     t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "book"
+    t.integer "book_id"
   end
 
   create_table "users", force: :cascade do |t|
