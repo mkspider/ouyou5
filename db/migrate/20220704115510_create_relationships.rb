8ae1e0d7ca0b23c4322b85619bc189fdb666a9b8
EC2 Default User <ec2-user@ip-172-31-18-135.ap-northeast-1.compute.internal>
2022-07-05 09:47:19 +0000

    4th

---
+++ db/migrate/20220704115510_create_relationships.rb
@@ -0,0 +1,10 @@
+class CreateRelationships < ActiveRecord::Migration[6.1]
+  def change
+    create_table :relationships do |t|
+      t.references :follower, null: false, foreign_key: {to_table: :users}
+      t.references :followed, null: false, foreign_key: {to_table: :users}
+      t.timestamps
+    end
+    add_index :follow_relationships, [:follower_id, :following_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_013743) do
+ActiveRecord::Schema.define(version: 2022_07_04_115510) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -65,6 +65,24 @@ ActiveRecord::Schema.define(version: 2022_07_02_013743) do
     t.integer "book_id"
   end
 
+  create_table "follow_relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_follow_relationships_on_followed_id"
+    t.index ["follower_id"], name: "index_follow_relationships_on_follower_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id", null: false
+    t.integer "followed_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -81,4 +99,8 @@ ActiveRecord::Schema.define(version: 2022_07_02_013743) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "follow_relationships", "followeds"
+  add_foreign_key "follow_relationships", "followers"
+  add_foreign_key "relationships", "followeds"
+  add_foreign_key "relationships", "followers"
 end
