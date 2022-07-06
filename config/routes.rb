d3c5b64024b9a9486887f0f3d7d9470f4305bddb
EC2 Default User <ec2-user@ip-172-31-18-135.ap-northeast-1.compute.internal>
2022-07-06 08:12:03 +0000

    kansei

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  get"search" => "searches#search_result"
    resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
      resources :book_comments, only: [:create, :destroy]
      resources :favorites, only: [:create, :destroy]
@@ -12,6 +13,8 @@ Rails.application.routes.draw do
   	get 'followings' => 'relationships#followings', as: 'followings'
   	get 'followers' => 'relationships#followers', as: 'followers'
   end
+  
+   
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
