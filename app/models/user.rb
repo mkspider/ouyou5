5f3ca7830747dd8b78ae2e97952685ea47b2b83f
EC2 Default User <ec2-user@ip-172-31-18-135.ap-northeast-1.compute.internal>
2022-07-07 12:17:36 +0000

    hidouki

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
+  has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
